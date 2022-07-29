9d63044771e0481efbbfc778d45cc28227079217
fonCki <alfonsoridao@gmail.com>
2022-07-29 09:24:51 +0200

    initial commit

---
+++ src/Box.js
@@ -0,0 +1,11 @@
+export class Box {
+    render() {
+        return (
+            <div>
+                <h1>User Information</h1>
+                <input/>
+                <button>Send</button>
+            </div>
+        )
+    }
+}
